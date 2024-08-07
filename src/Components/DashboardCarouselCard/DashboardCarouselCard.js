446aa19b709b7e575e9d171346bc01c2f25880cf
Rishab khamb <rishab.khamb@therunner.co.in>
2024-08-07 21:01:01 +0530

    fixed card

--- src/Components/DashboardCarouselCard/DashboardCarouselCard.js
+++ src/Components/DashboardCarouselCard/DashboardCarouselCard.js
@@ -4,28 +4,42 @@ import { orderData } from "../../store";
 const DashboardCarouselCard = () => {
     return <div id="dashboard-card-carousel" className='row show-on-small'>
         <div className='col-12'>
-            <div id="carouselExampleAutoplaying" className="carousel slide" data-bs-ride="carousel">
-                <div className="carousel-inner">
-                    {orderData.map((item, index) => (
-                        <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
-                            <OrderCard
-                                key={index}
-                                background={item.background}
-                                iconBackground={item.iconBackground}
-                                icon={item.icon}
-                                iconColor={item.iconColor}
-                                badgeIcon={item.badgeIcon}
-                                badgeColor={item.badgeColor}
-                                title={item.title}
-                                value={item.value}
-                                percentage={item.percentage}
-                                percentageColor={item.percentageColor}
-                                percentageIcon={item.percentageIcon}
-                            />
-                        </div>
-                    ))}
-                </div>
-            </div>
+        <div id="carouselExampleIndicators" className="carousel slide" data-bs-ride="carousel">
+  <div className="carousel-indicators">
+    {orderData.map((item, index) => (
+      <button 
+        key={index}
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide-to={index}
+        className={index === 0 ? "active" : ""}
+        aria-current={index === 0 ? "true" : "false"}
+        aria-label={`Slide ${index + 1}`}
+      ></button>
+    ))}
+  </div>
+  <div className="carousel-inner">
+    {orderData.map((item, index) => (
+      <div key={index} className={`carousel-item ${index === 0 ? 'active' : ''}`}>
+        <OrderCard
+          key={index}
+          background={item.background}
+          iconBackground={item.iconBackground}
+          icon={item.icon}
+          iconColor={item.iconColor}
+          badgeIcon={item.badgeIcon}
+          badgeColor={item.badgeColor}
+          title={item.title}
+          value={item.value}
+          percentage={item.percentage}
+          percentageColor={item.percentageColor}
+          percentageIcon={item.percentageIcon}
+        />
+      </div>
+    ))}
+  </div>
+</div>
+
         </div>
     </div>
 }
